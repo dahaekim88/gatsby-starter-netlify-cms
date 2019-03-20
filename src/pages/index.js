97bb2c80bc12d5048342314aa81d825a58a79f34
dahaekim88 <dhkim1014@gmail.com>
2019-03-20 15:38:00 +0900

    Test iamport payment window

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,66 @@
 import React from "react"
 import { Link } from "gatsby"
 import { getUser, isLoggedIn } from "../services/auth"
+import Helmet from "react-helmet"
 
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <h1>Hello {isLoggedIn() ? getUser().name : "world"}!</h1>
-    <p>
-      {isLoggedIn() ? (
-        <>
-          You are logged in, so check your{" "}
-          <Link to="/app/profile">profile</Link>
-        </>
-      ) : (
-        <>
-          You should <Link to="/app/login">log in</Link> to see restricted
-          content
-        </>
-      )}
-    </p>
-  </Layout>
-)
+export default () => {
+  const handleIMPSubmit = async () => {
+    const IMP = window.IMP
+    IMP.init("imp83932662")
+
+    IMP.request_pay(
+      {
+        pg: "html5_inicis",
+        pay_method: "card",
+        merchant_uid: "ORD20180131-0000011",
+        name: "노르웨이 회전 의자",
+        amount: 64900,
+        buyer_email: "gildong@gmail.com",
+        buyer_name: "홍길동",
+        buyer_tel: "010-4242-4242",
+        buyer_addr: "서울특별시 강남구 신사동",
+        buyer_postcode: "01181",
+      },
+      rsp => {
+        // callback
+        if (rsp.success) {
+          console.log("성공")
+        } else {
+          alert("결제에 실패하였습니다 다시 시도해주세요")
+        }
+      }
+    )
+  }
+
+  return (
+    <Layout>
+      <Helmet>
+        <script
+          type="text/javascript"
+          src="https://code.jquery.com/jquery-1.12.4.min.js"
+        />
+        <script
+          type="text/javascript"
+          src="https://cdn.iamport.kr/js/iamport.payment-1.1.5.js"
+        />
+      </Helmet>
+      <h1>Hello {isLoggedIn() ? getUser().name : "world"}!</h1>
+      <p>
+        {isLoggedIn() ? (
+          <>
+            You are logged in, so check your{" "}
+            <Link to="/app/profile">profile</Link> or
+            <button onClick={handleIMPSubmit}>Spend your money</button>
+          </>
+        ) : (
+          <>
+            You should <Link to="/app/login">log in</Link> to see restricted
+            content
+          </>
+        )}
+      </p>
+    </Layout>
+  )
+}
