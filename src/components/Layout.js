dd20b54df818b7d90a81ffc8789efa39d0a76f82
dahaekim88 <dhkim1014@gmail.com>
2019-04-16 17:18:47 +0900

    Import antd css and global styles with gatsby-browser config file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,64 +1,9 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import { createGlobalStyle } from "styled-components"
 
 import SEO from "../components/reusable/SEO"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-
-import lovelo from "../assets/fonts/Lovelo_Black.ttf"
-import { sizes } from "../constants"
-
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: Lovelo;
-    src: url(${lovelo});
-    font-weight: 900;
-  }
-
-  html {
-    font-size: 10px;
-
-    @media screen and (max-width: ${sizes.b_tablet}px) {
-      font-size: 8px;
-    }
-  }
-
-  html, body {
-    font-family: 'NanumSquare', sans-serif;
-  }
-
-  h2 {
-    margin: 1rem 0;
-  }
-
-  p {
-    font-size: 14px;
-  }
-
-  input[type=password] {
-    font-family: sans-serif
-  }
-
-  .carousel-control {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-image: none !important;
-  }
-
-  .container {
-    @media screen and (max-width: ${sizes.b_tablet}px) {
-      padding-left: 0;
-      padding-right: 0;
-    }
-  }
-
-  .navbar-header {
-    margin-left: 0 !important;
-    margin-right: 0 !important;
-  }
-`
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
@@ -76,7 +21,6 @@ const TemplateWrapper = ({ children }) => (
     `}
     render={data => (
       <div>
-        <GlobalStyle />
         <SEO metadata={data.site.siteMetadata} />
         <Navbar />
         <div>{children}</div>
