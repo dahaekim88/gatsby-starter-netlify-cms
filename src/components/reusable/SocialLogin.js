f1ff9c7c4115e0842662d04195603d05a2d97478
tkhwang <tkhwang@gmail.com>
2019-04-22 11:27:18 +0900

    [DEV] Kakao Social Login

--- src/components/reusable/SocialLogin.js
+++ src/components/reusable/SocialLogin.js
@@ -6,8 +6,10 @@ import { FormButton } from "../styled"
 import config from "../../../.config"
 import { facebook, google, kakao, kakaoFont, naver } from "../../constants"
 
-const urlForFacebookLogin = `${config.SERVER_URL}/auth/facebook`
-const urlForGoogleLogin = `${config.SERVER_URL}/auth/google`
+// const urlForFacebookLogin = `${config.SERVER_URL}/auth/facebook`
+// const urlForGoogleLogin = `${config.SERVER_URL}/auth/google`
+
+const getSocialLoginUrl = social => `${config.SERVER_URL}/auth/${social}`
 
 const override = css`
   display: block;
@@ -27,7 +29,7 @@ const SocialLogin = () => {
         </div>
       ) : (
         <React.Fragment>
-          <a href={urlForFacebookLogin}>
+          <a href={getSocialLoginUrl("facebook")}>
             <FormButton
               type="button"
               background={facebook}
@@ -37,17 +39,17 @@ const SocialLogin = () => {
               페이스북으로 로그인
             </FormButton>
           </a>
-          <a href={urlForGoogleLogin}>
+          <a href={getSocialLoginUrl("google")}>
             <FormButton type="button" background={google} color="#fff">
               구글로 로그인
             </FormButton>
           </a>
-          <a href={urlForFacebookLogin}>
+          <a href={getSocialLoginUrl("kakao")}>
             <FormButton type="button" background={kakao} color={kakaoFont}>
               카카오로 로그인
             </FormButton>
           </a>
-          <a href={urlForGoogleLogin}>
+          <a href={getSocialLoginUrl("naver")}>
             <FormButton type="button" background={naver} color="#fff">
               네이버로 로그인
             </FormButton>
